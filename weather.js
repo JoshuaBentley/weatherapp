c97c4263741eda763abbbd1cc2857fcce1f2deb0
JoshuaBentley <joshuabentley911@gmail.com>
2022-12-18 11:54:53 -0700

    you now get an icon matching the weather for the city you searched

--- weather.js
+++ weather.js
@@ -5,41 +5,42 @@ var weather = {
            if (response.ok) {
              return response.json();
            } else {
-             throw new Error("NETWORK RESPONSE ERROR");
+             throw new Error('NETWORK RESPONSE ERROR');
            }
          })
          .then(data => {
            console.log(data);
            this.displayWeather(data)
          })
-         .catch((error) => console.error("FETCH ERROR:", error));
+         .catch((error) => console.error('FETCH ERROR:', error));
        
 },
     displayWeather: function(data) {
-       var location = data.city.name
+       var locationCity = data.city.name
+       var locationCountry = data.city.country 
        var temp = data.list[0].main.temp;
        var conditions = data.list[1].weather[0].description;
        var minTemp = data.list[0].main.temp_min;
        var maxTemp = data.list[0].main.temp_max;
        var windSpeed = data.list[3].wind.speed;
-      //  var icon = data.list[1].weather[0].icon
+       var icon = data.list[1].weather[0].icon
        console.log(conditions);
-       document.querySelector(".location1").innerText = location;
-       document.querySelector(".tempature1").innerText = temp + "°F"; 
-       document.querySelector(".weatherConditions1").innerText = conditions;
-       document.querySelector(".minTemp1").innerText = minTemp + "°F";
-       document.querySelector(".maxTemp1").innerText = maxTemp + "°F";
-       document.querySelector(".windSpeed1").innerText = windSpeed + "mph";
-      //  document.querySelector(".icon") = " http://openweathermap.org/img/wn/" + icon;
+       document.querySelector('.location1').innerText = locationCity + ', ' + locationCountry;
+       document.querySelector('.tempature1').innerText = temp + '°F'; 
+       document.querySelector('.weatherConditions1').innerText = conditions;
+       document.querySelector('.minTemp1').innerText = minTemp + '°F';
+       document.querySelector('.maxTemp1').innerText = maxTemp + '°F';
+       document.querySelector('.windSpeed1').innerText = windSpeed + 'mph';
+       document.querySelector('img').src = 'http://openweathermap.org/img/wn/' + icon + "@2x.png";
 
     },
     search: function() {
-        // var search = document.querySelector(".search");
-        this.getWeather(document.querySelector("input").value);
-        console.log("working")
+        // var search = document.querySelector('.search');
+        this.getWeather(document.querySelector('input').value);
+        console.log('working')
     }
 }
 
-document.querySelector(".searchButton").addEventListener("click", function () {
+document.querySelector('.searchButton').addEventListener('click', function () {
   weather.search()
 })
